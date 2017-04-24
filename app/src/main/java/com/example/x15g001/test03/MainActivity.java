ef7a2c82e839a5e8a086fe5cb789fee7827e5313
WIN_NT\x15g001 <x15g001@chiba-fjb.ac.jp>
2017-04-24 11:56:34 +0900

    1

---
+++ app/src/main/java/com/example/x15g001/test03/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.x15g001.test03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
